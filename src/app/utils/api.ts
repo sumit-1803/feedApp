59efd439040653fa3f1c8f478f27774e8d33c9b3
Sumit Kumar <sumit182003@gmail.com>
2024-09-11 21:41:46 +0530

    corrections

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,7 +1,7 @@
 // utils/api.ts
 export async function fetchItems(category: string | null) {
     const query = category ? `?category=${category}` : '';
-    const response = await fetch(`https://feedappbackendd-cnunkwxp7-sumit-1803s-projects.vercel.app/api/items${query}`);
+    const response = await fetch(`https://feedappbackendd-cg0pxqu9a-sumit-1803s-projects.vercel.app/api/items${query}`);
     
     // Check the response status and content type
     if (!response.ok) {
