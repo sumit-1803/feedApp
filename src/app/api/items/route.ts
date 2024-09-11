cab0e213adaeabe9a2194b79d4713012bb31fc7e
Sumit Kumar <sumit182003@gmail.com>
2024-09-11 23:48:07 +0530

    new

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -1,21 +1,25 @@
 import { NextResponse } from 'next/server';
 import mongoose from 'mongoose';
-import Item from '../../../models/Item'; 
+import Item from '../../../models/Item';
 
 const connectToDatabase = async () => {
-  if (mongoose.connection.readyState === 1) return;
-  await mongoose.connect(process.env.MONGODB_URI || 'your-mongodb-uri'); 
+    if (mongoose.connection.readyState === 1) return;
+    await mongoose.connect(process.env.MONGODB_URI || 'your-mongodb-uri');
 };
 
 export async function GET(request: Request) {
-  try {
-    await connectToDatabase();
-    const url = new URL(request.url);
-    const category = url.searchParams.get('category');
-    const filter = category ? { category } : {}; 
-    const items = await Item.find(filter);
-    return NextResponse.json(items);
-  } catch (err: Error | any) {
-    return NextResponse.json({"error": err.message}, { status: 500 });
-  }
+    try {
+        await connectToDatabase();
+        const url = new URL(request.url);
+        const category = url.searchParams.get('category');
+        const filter = category ? { category } : {};
+        const items = await Item.find(filter);
+        return NextResponse.json(items);
+    }  catch (err: unknown) {
+        if (err instanceof Error) {
+            console.error("error:", err.message);
+        } else {
+            console.error("An unknown error occurred");
+        }
+    }
 }
